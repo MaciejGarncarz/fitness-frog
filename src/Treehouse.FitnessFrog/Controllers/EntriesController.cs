4da0ccbd0b3b936f36c143c15d371a0a248b2620
Maciej Garncarz <maciej.garncarz@microsoft.wsei.edu.pl>
2017-06-04 16:11:39 +0200

    Added confirmation messages using TempData

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -59,6 +59,8 @@ namespace Treehouse.FitnessFrog.Controllers
             {
                 _entriesRepository.AddEntry(entry);
 
+                TempData["Message"] = "Your entry was successfully added!";
+
                 return RedirectToAction("Index");
             }
 
@@ -98,6 +100,9 @@ namespace Treehouse.FitnessFrog.Controllers
             if (ModelState.IsValid)
             {
                 _entriesRepository.UpdateEntry(entry);
+
+                TempData["Message"] = "Your entry was successfully updated!";
+
                 return RedirectToAction("Index");
             }
 
@@ -126,6 +131,7 @@ namespace Treehouse.FitnessFrog.Controllers
         public ActionResult Delete (int id)
         { 
             _entriesRepository.DeleteEntry(id);
+            TempData["Message"] = "Your entry was successfully deleted!";
 
             return RedirectToAction("Index");
         }
